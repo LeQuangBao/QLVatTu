d128fd92e730cb02b8123fd1023a8b9b2da28714
Lê Quang Bảo <lequangbao@gmail.com>
2016-10-30 10:58:18 +0700

    đã tạo Model (dll) và CSDL đã có testProject dùng để test các module/function Chưa tạo giao diện

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -11,8 +11,16 @@ namespace TestProject {
             using (var context = new QuanLyVatTuContext()) {
                 var loai = from l in context.Loai
                            select l;
+                
                 foreach(Loai l in loai) {
                     Console.WriteLine(l.MaLoai.ToString());
+                }
+
+                var thietBi = from tb in context.ThietBi
+                              where tb.MaLoai == 0
+                              select tb;
+                foreach(ThietBi tb in thietBi) {
+                    Console.WriteLine(tb.NgayDuaVaoSuDung.ToString());
                 }
             }
         }
