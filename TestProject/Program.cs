d161575bd540758744522933ae0c70262d5a4950
Lê Quang Bảo <lequangbao@gmail.com>
2016-10-30 10:10:59 +0700

    Add project files.

---
+++ TestProject/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DF_Model;
+
+namespace TestProject {
+    class Program {
+        static void Main(string[] args) {
+            using (var context = new QuanLyVatTuContext()) {
+                var loai = from l in context.Loai
+                           select l;
+                foreach(Loai l in loai) {
+                    Console.WriteLine(l.MaLoai.ToString());
+                }
+            }
+        }
+    }
+}
