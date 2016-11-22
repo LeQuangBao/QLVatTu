f511c77f85204afa1ba353b3ec7fd22868f712b6
Lê Quang Bảo <lequangbao@gmail.com>
2016-11-22 20:40:36 +0700

    Đã tạo controller, view cho Phiếu Nhập. Các chức năng CRUD (Create, Read, Update, Delete) đã hoạt động nhưng chưa thể tạo chi tiết phiếu nhập

---
+++ WebForm/Controllers/PhieuNhapController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Model;
+
+namespace WebForm.Controllers
+{
+    public class PhieuNhapController : Controller
+    {
+        private Context db = new Context();
+
+        // GET: PhieuNhap
+        public ActionResult Index()
+        {
+            var phieuNhap = db.PhieuNhap.Include(p => p.NhaCungCap);
+            return View(phieuNhap.ToList());
+        }
+
+        // GET: PhieuNhap/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PhieuNhap phieuNhap = db.PhieuNhap.Find(id);
+            if (phieuNhap == null)
+            {
+                return HttpNotFound();
+            }
+            return View(phieuNhap);
+        }
+
+        // GET: PhieuNhap/Create
+        public ActionResult Create()
+        {
+            ViewBag.MaNhaCungCap = new SelectList(db.NhaCungCap, "MaNhaCungCap", "TenNhaCungCap");
+            return View();
+        }
+
+        // POST: PhieuNhap/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaPhieuNhap,MaNhaCungCap,NgayNhap")] PhieuNhap phieuNhap)
+        {
+            if (ModelState.IsValid)
+            {
+                db.PhieuNhap.Add(phieuNhap);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.MaNhaCungCap = new SelectList(db.NhaCungCap, "MaNhaCungCap", "TenNhaCungCap", phieuNhap.MaNhaCungCap);
+            return View(phieuNhap);
+        }
+
+        // GET: PhieuNhap/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PhieuNhap phieuNhap = db.PhieuNhap.Find(id);
+            if (phieuNhap == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.MaNhaCungCap = new SelectList(db.NhaCungCap, "MaNhaCungCap", "TenNhaCungCap", phieuNhap.MaNhaCungCap);
+            return View(phieuNhap);
+        }
+
+        // POST: PhieuNhap/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaPhieuNhap,MaNhaCungCap,NgayNhap")] PhieuNhap phieuNhap)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(phieuNhap).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.MaNhaCungCap = new SelectList(db.NhaCungCap, "MaNhaCungCap", "TenNhaCungCap", phieuNhap.MaNhaCungCap);
+            return View(phieuNhap);
+        }
+
+        // GET: PhieuNhap/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PhieuNhap phieuNhap = db.PhieuNhap.Find(id);
+            if (phieuNhap == null)
+            {
+                return HttpNotFound();
+            }
+            return View(phieuNhap);
+        }
+
+        // POST: PhieuNhap/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            PhieuNhap phieuNhap = db.PhieuNhap.Find(id);
+            db.PhieuNhap.Remove(phieuNhap);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
